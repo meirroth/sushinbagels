b1b8a7ea5345470a8ecb25d52bcca8fdaa284365
Meir Roth <12494197+meirroth@users.noreply.github.com>
2021-08-24 06:54:10 +0300

    noindex

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,6 +19,7 @@ export default {
     },
     meta: [
       { charset: 'utf-8' },
+      { name: 'robots', content: 'noindex' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
