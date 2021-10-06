d7183f1506e7cd8974ad832a0e46bbff2c1281ee
Meir Roth <12494197+meirroth@users.noreply.github.com>
2021-10-07 02:57:07 +0300

    Update gallery.vue

--- pages/gallery.vue
+++ pages/gallery.vue
@@ -23,7 +23,8 @@
           >
             <Statically
               :src="image.thumb"
-              width="432"
+              width="437"
+              height="291"
               :loading="i > 9 ? 'lazy' : null"
               class="absolute object-cover w-full h-full"
             />
